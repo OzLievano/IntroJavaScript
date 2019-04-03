af6c0e53308424ab0079ccb59b872029f12d67a7
Osvaldo Lievano <Osvaldoalievano@gmail.com>
2019-04-03 00:26:44 -0700

    update

--- main.js
+++ main.js
@@ -1,11 +1,5 @@
-
-$(document).ready(function()) {
+console.log('main.js loaded');
+$(document).ready(function()){
   'use strict';
-  paper.install(window);
-  paper.setup(document.getElementById('mainCanvas'));
-
-  var c = Shape.Circle(200,200,50);
-  c.fillColor = 'green';
-  paper.view.draw();
-
+  console.log('main.js loaded')
 });
