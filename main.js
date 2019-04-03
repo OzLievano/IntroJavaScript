f78bdc5b88df83935b17a33cc994c686829ff128
Osvaldo Lievano <Osvaldoalievano@gmail.com>
2019-04-02 23:08:34 -0700

    this commit installs paper.js and attaches it to the canvas

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 /* Styles go here. */
 $(document).ready(function()) {
   'use strict';
+  paper.install(window);
+  paper.setup(document.getElementById('mainCanvas'));
+
+  // // TODO:
+
+  paper.view.draw();
+  
   console.log('main.js loaded');
 });
